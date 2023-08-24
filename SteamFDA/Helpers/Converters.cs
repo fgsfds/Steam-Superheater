d6aa2875b2855a6cfe59a880d523b3374250ed29
fgsfds <4870330+fgsfds@users.noreply.github.com>
2023-08-24 18:47:42 +0500

    styles tweaks

--- SteamFDA/Helpers/Converters.cs
+++ SteamFDA/Helpers/Converters.cs
@@ -6,56 +6,59 @@ using System.Globalization;
 
 namespace SteamFDA.Helpers
 {
+    /// <summary>
+    /// Converts bool to one of two strings
+    /// </summary>
     public class BoolToStringConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var strings = ((string)parameter).Split(",");
             return (bool)value ? strings[0] : strings[1];
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException("ConvertBack method for BoolToStringConverter is not implemented.");
         }
     }
 
+    /// <summary>
+    /// Reverses bool value
+    /// </summary>
     public class ReverseBool : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return !(bool)value;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return !(bool)value;
         }
     }
 
+    /// <summary>
+    /// Converts bool to color from a string of two colors separated by ;
+    /// </summary>
     public class BoolToColorConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var pars = ((string)parameter).Split(";");
 
             return (bool)value ? new SolidColorBrush(Color.Parse(pars[0])) : new SolidColorBrush(Color.Parse(pars[1]));
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException("ConvertBack method for BoolToColorConverter is not implemented.");
         }
     }
 
     /// <summary>
-    /// <para>
-    /// Converts a string path to a bitmap asset.
-    /// </para>
-    /// <para>
-    /// The asset must be in the same assembly as the program. If it isn't,
-    /// specify "avares://<assemblynamehere>/" in front of the path to the asset.
-    /// </para>
+    /// Converts path to image to Bitmap
     /// </summary>
     public class ImagePathToBitmapConverter : IValueConverter
     {
