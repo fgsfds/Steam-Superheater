64ac5e988a99424997239f808da04c47e5a7ccbc
fgsfds <4870330+fgsfds@users.noreply.github.com>
2023-08-24 08:32:27 +0500

    formatting support in description

--- SteamFDA/UserControls/MainLists.axaml.cs
+++ SteamFDA/UserControls/MainLists.axaml.cs
@@ -1,4 +1,11 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Styling;
+using SteamFDTCommon.Entities;
+using System.Diagnostics;
+using System.Reflection.Emit;
 
 namespace SteamFDA.UserControls
 {
@@ -7,6 +14,57 @@ namespace SteamFDA.UserControls
         public MainLists()
         {
             InitializeComponent();
+        }
+
+
+        private void FixSelected(object sender, SelectionChangedEventArgs e)
+        {
+            var style = Application.Current.Resources.TryGetValue("ResourceKey", out var value);
+
+            stack.Children.Clear();
+
+            if (((ListBox)sender).SelectedItem is null)
+            {
+                return;
+            }
+            var description = ((FixEntity)((ListBox)sender).SelectedItem).Description;
+
+            var splitDescription = description.Split('\n');
+
+            foreach(var item in splitDescription )
+            {
+                if (item.StartsWith("*"))
+                {
+                    var text = item[1..];
+                    stack.Children.Add((
+                        new TextBlock() { Text = text, FontWeight = FontWeight.Black, TextWrapping = TextWrapping.Wrap })
+                        );
+                    continue;
+                }
+                else if (item.StartsWith("http"))
+                {
+                    var button = new Button
+                    {
+                        Content = item
+                    };
+
+                    button.Click += ButtonClick;
+
+                    stack.Children.Add(button);
+                    continue;
+                }
+
+
+
+                stack.Children.Add((new TextBlock() { Text = item, TextWrapping = TextWrapping.Wrap }));
+            }
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            var url = ((Button)sender).Content.ToString();
+
+            Process.Start("explorer.exe", url);
         }
     }
 }
