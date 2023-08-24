78eee0e575b0c348a2f721a51b508bfa6374e11e
fgsfds <4870330+fgsfds@users.noreply.github.com>
2023-08-24 08:05:02 +0500

    formatting

--- SteamFDA/UserControls/MainLists.axaml.cs
+++ SteamFDA/UserControls/MainLists.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using SteamFDTCommon.Entities;
+using System.Diagnostics;
 
 namespace SteamFDA.UserControls
 {
@@ -7,6 +11,56 @@ namespace SteamFDA.UserControls
         public MainLists()
         {
             InitializeComponent();
+        }
+
+
+        private void FixSelected(object sender, SelectionChangedEventArgs e)
+        {
+            stack.Children.Clear();
+
+            if (((ListBox)sender).SelectedItem is null)
+            {
+                return;
+            }
+            var description = ((FixEntity)((ListBox)sender).SelectedItem).Description;
+
+            var splitDescription = description.Split('\n');
+
+            foreach(var item in splitDescription )
+            {
+                if (item.StartsWith("*"))
+                {
+                    var text = item[1..];
+                    stack.Children.Add((
+                        new TextBlock() { Text = text, FontWeight = FontWeight.Black, TextWrapping = TextWrapping.Wrap })
+                        );
+                    continue;
+                }
+                else if (item.StartsWith("http"))
+                {
+                    var button = new Button
+                    {
+                        Content = item,
+                        Background = new SolidColorBrush(Colors.White) { Opacity = 0 }
+                    };
+
+                    button.Click += ButtonClick;
+
+                    stack.Children.Add(button);
+                    continue;
+                }
+
+
+
+                stack.Children.Add((new TextBlock() { Text = item, TextWrapping = TextWrapping.Wrap }));
+            }
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            var url = ((Button)sender).Content.ToString();
+
+            Process.Start("explorer.exe", url);
         }
     }
 }
