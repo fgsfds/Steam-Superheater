c19428673168af98455cd51ce8c864d09567565f
fgsfds <4870330+fgsfds@users.noreply.github.com>
2023-08-26 23:31:58 +0500

    red new version from github

--- SteamFDA/ViewModels/AboutViewModel.cs
+++ SteamFDA/ViewModels/AboutViewModel.cs
@@ -1,34 +1,37 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using SteamFDCommon.Updater;
+using System.Reflection;
 
 namespace SteamFDA.ViewModels
 {
-public class AboutViewModel : ObservableObject
-{
-public bool IsUpdateAvailable { get; set; }
-
-public bool IsInProgress { get; set; }
-
-public IRelayCommand CheckForUpdatesCommand { get; set; }
-
-public AboutViewModel()
-{
-IsUpdateAvailable = false;
-IsInProgress = false;
-
-CheckForUpdatesCommand = new RelayCommand(() =>
+    public class AboutViewModel : ObservableObject
     {
-    IsInProgress = true;
-    OnPropertyChanged(nameof(IsInProgress));
-    CheckForUpdatesCommand.NotifyCanExecuteChanged();
-    },
-() => IsInProgress is false
-        );
+        public bool IsUpdateAvailable { get; set; }
+
+        public bool IsInProgress { get; set; }
+
+        public IRelayCommand CheckForUpdatesCommand { get; set; }
+
+        public AboutViewModel()
+        {
+            IsUpdateAvailable = false;
+            IsInProgress = false;
+
+            CheckForUpdatesCommand = new RelayCommand(async () =>
+            {
+                IsInProgress = true;
+                await GithubReleasesProvider.ReadReleasesAsync(Assembly.GetEntryAssembly().GetName().Version);
+                OnPropertyChanged(nameof(IsInProgress));
+                CheckForUpdatesCommand.NotifyCanExecuteChanged();
+            },
+            () => IsInProgress is false
+                    );
         }
 
         private void CheckForUpdates()
         {
 
         }
-        }
-        }
+    }
+}
---
+++ SteamFDCommon/Updater/GithubReleasesProvider.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace SteamFDCommon.Updater
+{
+    public class GithubReleasesProvider
+    {
+        public static async Task ReadReleasesAsync(Version currentVersion)
+        {
+            GithubReleaseEntity.Root latestVersion;
+            IEnumerable<GithubReleaseEntity.Root> newVersions;
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.UserAgent.ParseAdd("fgsfds");
+
+                var a = await client.GetStringAsync("https://api.github.com/repos/fgsfds/Test-Repo/releases");
+
+                var cc = JsonConvert.DeserializeObject<List<GithubReleaseEntity.Root>>(a);
+
+                latestVersion = cc.Last();
+
+                newVersions = cc.Where(x => new Version(x.tag_name) > currentVersion);
+            }
+        }
+    }
+}
