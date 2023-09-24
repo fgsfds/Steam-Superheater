3c27d42eb97225b762d7a5e57353e0c485d649fa
fgsfds <4870330+fgsfds@users.noreply.github.com>
2023-09-25 00:08:04 +0500

    about tab and icon

---
+++ src/WPF/Superheater/Pages/AboutPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using SteamFD.ViewModels;
+using SteamFDCommon.DI;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SteamFD.Pages
+{
+    /// <summary>
+    /// Interaction logic for AboutPage.xaml
+    /// </summary>
+    public partial class AboutPage : UserControl
+    {
+        private readonly AboutViewModel _avm;
+
+        public AboutPage()
+        {
+            _avm = BindingsManager.Instance.GetInstance<AboutViewModel>();
+
+            DataContext = _avm;
+
+            InitializeComponent();
+        }
+
+        private void SteamClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer.exe", "https://steamcommunity.com/id/hasnogames/");
+        }
+
+        private void GitHubClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer.exe", "https://github.com/fgsfds/Steam-Superheater");
+        }
+
+        private void GitHubIssuesClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer.exe", "https://github.com/fgsfds/Steam-Superheater/issues/new");
+        }
+
+        private void ShowChangelogClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer.exe", "https://github.com/fgsfds/Steam-Superheater/releases");
+        }
+    }
+}
