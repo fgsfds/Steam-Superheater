608a772df2734cfc4dccb9190a54ae7e993eef95
fgsfds <4870330+fgsfds@users.noreply.github.com>
2023-09-28 22:03:41 +0500

    added rudimentary logger

---
+++ src/Common/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class Logger
+    {
+        private static string _logFile => Path.Combine(Directory.GetCurrentDirectory(), "superheater.log");
+
+        private static object _lock = new();
+
+        public static void Log(string message)
+        {
+            lock (_lock)
+            {
+                File.AppendAllText(_logFile, message + Environment.NewLine);
+            }
+        }
+    }
+}
