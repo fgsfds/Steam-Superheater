73ca0181562ebfa2f5c0fa0721a8978732d6d5c5
fgsfds <4870330+fgsfds@users.noreply.github.com>
2023-09-30 08:01:32 +0500

    supported oses checkboxes

---
+++ src/Common/OSEnum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [Flags]
+    public enum OSEnum
+    {
+        Windows = 2,
+        Linux = 4
+    }
+
+    public static class OSEnumhelper
+    {
+        public static OSEnum AddFlag(this OSEnum osenum, OSEnum flag)
+        {
+            return osenum |= flag;
+        }
+
+        public static OSEnum RemoveFlag(this OSEnum osenum, OSEnum flag)
+        {
+            return osenum &= ~flag;
+        }
+    }
+}
