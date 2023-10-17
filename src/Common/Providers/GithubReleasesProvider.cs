1a36df314e67b5714dc2cc0028631da9cbe23753
fgsfds <4870330+fgsfds@users.noreply.github.com>
2023-10-17 17:46:08 +0500

    don't update to prereleases

--- src/Common/Providers/GithubReleasesProvider.cs
+++ src/Common/Providers/GithubReleasesProvider.cs
@@ -7,7 +7,7 @@ namespace Common.Providers
     public static class GitHubReleasesProvider
     {
         /// <summary>
-        /// Return a list of releases from github repo that have higher version than the current
+        /// Return a list of releases from GitHub repo that have higher version than the current
         /// </summary>
         /// <param name="currentVersion">current release version</param>
         public static async Task<IEnumerable<UpdateEntity>> GetNewerReleasesListAsync(Version currentVersion)
@@ -18,10 +18,12 @@ namespace Common.Providers
 
                 var a = await client.GetStringAsync(Consts.GitHubReleases);
 
-                var cc = JsonSerializer.Deserialize<List<GitHubRelease>>(a)
+                var releases = JsonSerializer.Deserialize<List<GitHubRelease>>(a)
                     ?? throw new Exception("Error while deserializing GitHub releases");
 
-                var updates = cc.ConvertAll(x => new UpdateEntity(
+                releases = releases.Where(x => x.draft is false && x.prerelease is false).ToList();
+
+                var updates = releases.ConvertAll(x => new UpdateEntity(
                     new Version(x.tag_name),
                     x.body,
                     new Uri(x.assets.Where(x => x.name.EndsWith("win-x64.zip")).First().browser_download_url)
