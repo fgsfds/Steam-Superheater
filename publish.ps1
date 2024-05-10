182c18234b52b34ec0e6db558744aaf8a6355517
fgsfds <4870330+fgsfds@users.noreply.github.com>
2024-05-10 19:13:45 +0500

    added publishing scripts

---
+++ publish.ps1
@@ -0,0 +1,9 @@
+dotnet publish ".\src\Avalonia\Desktop\Avalonia.Desktop.csproj" -p:PublishProfile=Windows
+dotnet publish ".\src\Avalonia\Desktop\Avalonia.Desktop.csproj" -p:PublishProfile=Linux
+
+$version = (Get-Item .\publish\Superheater.exe).VersionInfo.FileVersion
+$version = $version.Substring(0,$string.Length-2)
+$version = $version.Replace('.','')
+
+Compress-Archive -Path ".\publish\Superheater.exe" -DestinationPath ".\publish\superheater_${string}_win-x64.zip"
+Compress-Archive -Path ".\publish\Superheater" -DestinationPath ".\publish\superheater_${string}_linux-x64.zip"
