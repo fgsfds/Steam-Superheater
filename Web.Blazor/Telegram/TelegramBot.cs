ee16a2994c83fb056d98c284ebb8c476fe67f381
fgsfds <4870330+fgsfds@users.noreply.github.com>
2024-07-31 07:00:04 +0500

    updated tg bot nuget

--- Web.Blazor/Telegram/TelegramBot.cs
+++ Web.Blazor/Telegram/TelegramBot.cs
@@ -8,8 +8,7 @@ namespace Web.Blazor.Telegram
 {
     public sealed class TelegramBot
     {
-        private readonly ITelegramBotClient _botClient;
-        private readonly ReceiverOptions _receiverOptions;
+        private readonly TelegramBotClient _bot;
         private readonly HttpClient _httpClient;
         private readonly ILogger<TelegramBot> _logger;
 
@@ -17,82 +16,53 @@ namespace Web.Blazor.Telegram
         private readonly string _token = Environment.GetEnvironmentVariable("TgToken")!;
         private readonly string _apiPassword = Environment.GetEnvironmentVariable("ApiPass")!;
 
-        public TelegramBot(HttpClient httpClient)
+        public TelegramBot(
+            HttpClient httpClient,
+            ILogger<TelegramBot> logger
+            )
         {
-            _botClient = new TelegramBotClient(_token);
             _httpClient = httpClient;
-
-            _receiverOptions = new ReceiverOptions
-            {
-                AllowedUpdates = [UpdateType.Message],
-                ThrowPendingUpdates = true,
-            };
+            _logger = logger;
+            _bot = new TelegramBotClient(_token, httpClient);
         }
 
         public async Task StartAsync()
         {
-            using var cts = new CancellationTokenSource();
+            var me = await _bot.GetMeAsync().ConfigureAwait(false);
 
-            _botClient.StartReceiving(UpdateHandler, ErrorHandler, _receiverOptions, cts.Token);
-
-            var me = await _botClient.GetMeAsync().ConfigureAwait(false);
+            _bot.OnError += OnError;
+            _bot.OnMessage += OnMessage;
 
             await SendMessageAsync("Server started").ConfigureAwait(false);
         }
 
-        public async Task SendMessageAsync(string text, string? id = null)
+        private async Task OnMessage(Message message, UpdateType type)
         {
-            id ??= _chatId;
+            _logger.LogInformation("Got message");
 
-            await _botClient.SendTextMessageAsync(
-                id,
-                text
-                ).ConfigureAwait(false);
-        }
-
-        private async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-        {
-            try
+            if (message.From!.Id.ToString() != _chatId)
             {
-                _logger.LogInformation($"Got update of type {update.Type.ToString()}");
-
-                switch (update.Type)
-                {
-                    case UpdateType.Message:
-                        {
-                            if (update.Message!.From!.Id.ToString() != _chatId)
-                            {
-                                await SendMessageAsync(
-                                    "You are not supposed to be here",
-                                    update.Message!.From!.Id.ToString()
-                                    ).ConfigureAwait(false);
-                            }
-
-                            if (update.Message!.Text!.Equals("Ping", StringComparison.OrdinalIgnoreCase))
-                            {
-                                _logger.LogInformation("Pong");
-                                await SendMessageAsync("Pong").ConfigureAwait(false);
-                            }
-                            else if (update.Message!.Text!.Equals("Check", StringComparison.OrdinalIgnoreCase))
-                            {
-                                _logger.LogInformation("Check message received");
-                                await _httpClient.PostAsJsonAsync($"https://superheater.fgsfds.link/api/fixes/check", _apiPassword, cancellationToken).ConfigureAwait(false);
-                            }
-
-                            return;
-                        }
-                }
+                await SendMessageAsync(
+                    "You are not supposed to be here",
+                    message!.From!.Id.ToString()
+                    ).ConfigureAwait(false);
             }
-            catch (Exception ex)
+
+            if (message!.Text!.Equals("Ping", StringComparison.OrdinalIgnoreCase))
             {
-                _logger.LogCritical(ex, "Critical telegram error");
-                await SendMessageAsync(ex.ToString()).ConfigureAwait(false);
+                _logger.LogInformation("Pong");
+                await SendMessageAsync("Pong").ConfigureAwait(false);
+            }
+            else if (message!.Text!.Equals("Check", StringComparison.OrdinalIgnoreCase))
+            {
+                _logger.LogInformation("Check message received");
+                await _httpClient.PostAsJsonAsync($"https://superheater.fgsfds.link/api/fixes/check", _apiPassword).ConfigureAwait(false);
             }
         }
 
-        private async Task ErrorHandler(ITelegramBotClient botClient, Exception error, CancellationToken cancellationToken)
+        private async Task OnError(Exception exception, HandleErrorSource source)
         {
-            var message = error switch
+            var message = exception switch
             {
                 ApiRequestException apiRequestException => $"""
                 Telegram API Error:
@@ -101,11 +71,21 @@ namespace Web.Blazor.Telegram
 
                 {apiRequestException.Message}
                 """,
-                _ => error.ToString()
+                _ => exception.ToString()
             };
 
             _logger.LogError(message);
             await SendMessageAsync(message).ConfigureAwait(false);
+        }
+
+        public async Task SendMessageAsync(string text, string? id = null)
+        {
+            id ??= _chatId;
+
+            await _bot.SendTextMessageAsync(
+                id,
+                text
+                ).ConfigureAwait(false);
         }
     }
 }
