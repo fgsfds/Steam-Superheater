084d06d8924c7f134bef0da86cd052beb064dae6
fgsfds <4870330+fgsfds@users.noreply.github.com>
2025-05-17 14:03:04 +0500

    got rid of AWSSDK

--- Web.Blazor/Helpers/S3Client.cs
+++ Web.Blazor/Helpers/S3Client.cs
@@ -1,28 +1,9 @@
-using Amazon.Runtime;
-using Amazon.S3;
-using Amazon.S3.Model;
-using Common.Helpers;
-
 namespace Web.Blazor.Helpers;
 
 public sealed class S3Client : IDisposable
 {
     private readonly string _key = Environment.GetEnvironmentVariable("S3Key")!;
     private readonly string _secretKey = Environment.GetEnvironmentVariable("S3SKey")!;
-
-    private readonly AmazonS3Client _s3client;
-
-    public S3Client()
-    {
-        AmazonS3Config config = new()
-        {
-            ServiceURL = string.Format("https://s3.timeweb.cloud"),
-            UseHttp = false
-        };
-
-        AWSCredentials creds = new BasicAWSCredentials(_key, _secretKey);
-        _s3client = new(creds, config);
-    }
 
     /// <summary>
     /// Get signed URL for file uploading
@@ -31,28 +12,9 @@ public sealed class S3Client : IDisposable
     /// <returns>Signed URL</returns>
     public string? GetSignedUrl(string file)
     {
-        GetPreSignedUrlRequest request = new()
-        {
-            BucketName = Consts.Bucket,
-            Key = file,
-            Expires = DateTime.Now.AddHours(1),
-            Verb = HttpVerb.PUT
-        };
-
-        string? url;
-
-        try
-        {
-            url = _s3client.GetPreSignedURL(request);
-        }
-        catch
-        {
-            return null;
-        }
-
-        return url;
+        return null;
     }
 
-    public void Dispose() => _s3client?.Dispose();
+    public void Dispose() { }
 }
 
